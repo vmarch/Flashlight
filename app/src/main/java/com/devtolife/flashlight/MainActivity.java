eea444d9d421bcdcd8d511d8f0ab64001b22193d
Vovas <marchenkoukr@gmail.com>
2018-09-22 13:42:09 +0300

    Added Camera permission for Build.VERSION newer 22.

--- app/src/main/java/com/devtolife/flashlight/MainActivity.java
+++ app/src/main/java/com/devtolife/flashlight/MainActivity.java
@@ -1,10 +1,14 @@
 package com.devtolife.flashlight;
 
-
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +19,8 @@ import com.devtolife.flashlight.privacy_policy.PrivacyPolicy;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int PERMISSIONS_REQUEST_SENDER = 1;
+    private static final int PERMISSIONS_REQUEST_SENDER_RESUME = 2;
     private ImageButton imgbtn;
     private Camera camera;
     private Camera.Parameters param;
@@ -33,15 +39,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hasFlash = getApplicationContext()
                 .getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-
         if (!hasFlash) {
-
             finish();
-        } else {
-            camera = Camera.open();
-            param = camera.getParameters();
-            turnOn();
         }
+        getPermis();
     }
 
     @Override
@@ -55,7 +56,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void turnOn() {
-
         if (camera != null && param != null) {
 
             param.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
@@ -83,8 +83,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             camera.release();
             camera = null;
             imgbtn.setImageResource(R.drawable.ic_btn_off);
+            on = false;
         }
     }
+
+    private void getPermis() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+                camera = Camera.open();
+                param = camera.getParameters();
+
+
+            } else {
+                requestPermissions(new String[]{Manifest.permission.CAMERA},
+                        PERMISSIONS_REQUEST_SENDER);
+            }
+        } else {
+            camera = Camera.open();
+            param = camera.getParameters();
+
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_SENDER && grantResults.length == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                getPermis();
+
+
+            } else {
+                requestPermissions(new String[]{Manifest.permission.CAMERA},
+                        PERMISSIONS_REQUEST_SENDER);
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        releaseCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        releaseCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        if (camera == null) {
+//
+//            camera = Camera.open();
+//            param = camera.getParameters();
+//            imgbtn.setImageResource(R.drawable.ic_btn_off);
+//            on = false;
+//        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        releaseCamera();
+        finish();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -114,32 +184,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseCamera();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        releaseCamera();
-        on = false;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (camera != null) {
-            return;
-        }
-        camera = Camera.open();
-    }
-
-    @Override
-    public void onBackPressed() {
-        releaseCamera();
-        finish();
-    }
 }
 
